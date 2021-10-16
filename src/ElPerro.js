da80d8c59d1f06773fcba685959f06f028180cc9
Alexis <alex10o@hotmail.com>
2021-10-16 04:44:44 -0300

    16-10 ordenado2

--- src/ElPerro.js
+++ src/ElPerro.js
@@ -5,24 +5,30 @@ export default function ElPerro(){
             <h2>Canis lupus familiaris</h2>
             <p> 
             El perro o perro doméstico (Canis lupus familiaris) es un mamífero carnívoro de la familia de los cánidos,
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Coat_types_3.jpg/800px-Coat_types_3.jpg" alt="" width="250" height="200" align="left" ></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Coat_types_3.jpg/800px-Coat_types_3.jpg" alt="" width="190" height="150" align="left" Style="border-radius: 40px"></img>
             que constituye una subespecie del lobo (Canis lupus). Un estudio publicado por la revista de divulgación científica Nature revela que,
             gracias al proceso dedomesticación, el organismo del perro se ha adaptado5 a cierta clase de alimentos, en este caso el almidón.
-            6 Su tamaño o talla, su forma y pelaje es muy diverso según la raza. Posee un oído y olfato muy desarrollados, siendo este último su principal órgano sensorial.
-            En las razas pequeñas puede alcanzar una longevidad de cerca de 20 años, con atención esmerada por parte del propietario, de otra forma su vida en promedio es alrededor de los 15 años.
+            6 Su tamaño o talla, su forma y pelaje es muy diverso según la raza. 
+            Posee un oído y olfato muy desarrollados, siendo este último su principal órgano sensorial.
+            En las razas pequeñas puede alcanzar una longevidad de cerca de 20 años, con atención esmerada por parte del propietario,
+            de otra forma su vida en promedio es alrededor de los 15 años.
             Se cree que el lobo gris, del que es considerado una subespecie, es el antepasado más inmediato. 
-            Las pruebas arqueológicas demuestran que el perro ha estado en convivencia cercana con los humanos desde hace al menos 9000 años, pero posiblemente desde hace 14 000 años. 
+            Las pruebas arqueológicas demuestran que el perro ha estado en convivencia cercana con los humanos desde hace al menos 9000 años, 
+            pero posiblemente desde hace 14 000 años. 
             Las pruebas fósiles demuestran que los antepasados de los perros modernos ya estaban asociados con los humanos hace 100 000 años. 
             Las investigaciones más recientes indican que el perro fue domesticado por primera vez en el este de Asia, posiblemente enChina;
             sin embargo, es incierto si todos los perros domésticos provienen de un mismo grupo o si el proceso de domesticación se repitió varias veces. 
             Hay aproximadamente 800 razas —más que de cualquier otro animal— que varían significativamente en tamaño, fisonomía y temperamento, presentando una gran variedad de colores y de tipos de pelo según la raza de perro. 
-            Tienen una gran relación con los humanos, para quien son animales de compañía, animales de guardia, perros de trabajo, perros de caza, perros de aguas, galgos de carrera, perros guía, perros pastores o perros boyeros por ejemplo. 
+            Tienen una gran relación con los humanos, para quien son animales de compañía, animales de guardia, perros de trabajo, perros de caza, perros de aguas, galgos de carrera,
+             perros guía, perros pastores o perros boyeros por ejemplo. 
             En 2001, se estimaba que había 400 millones de perros en el mundo.
             </p> 
-            <h2>Ancestros e historia de la domesticación </h2>
+            <br />
+            <br />
+            <h2>Ancestros e historia de la domesticación</h2>
             <p>En el siglo XXI, los investigadores han alcanzado un consenso casi absoluto acerca de que es muy posible que la domesticación del perro empezara más por la adaptación espontánea de este al acercarse a vivir junto al hombre que por la voluntad humana.8 
             Esto se debe a que vivir junto al hombre siempre fue ventajoso para el cánido. Un perro viviendo en una comunidad humana, aún en la Antigüedad, podía alimentarse
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Cave_canem.JPG/220px-Cave_canem.JPG" alt="" width="226" height="288" align="right"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Cave_canem.JPG/220px-Cave_canem.JPG" alt="" width="200" height="260" align="right" Style="border-radius: 10px"></img>
             con menos esfuerzo que uno salvaje, podía vivir en mejores condiciones disfrutando del afecto y cuidado humano. El perro es una subespecie doméstica del lobo, según la comparación de los mapas genéticos de ambas especies.
             9 La evidencia fósil más antigua de un perro domesticado fue encontrada en 2008 en la cueva Goyet de Bélgica, correspondiente a unos 31.700 años y al parecer asociado a la cultura auriñaciense.
             10 Hasta entonces las pruebas más antiguas se habían encontrado en Rusia, pertenecientes a hace 14.000 años (Eliseevich). 
